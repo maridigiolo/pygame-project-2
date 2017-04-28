d04fec06edf48a7eb74761748ceb79fba6b4a17c
Marina <maridigiolo@gmail.com>
2017-04-28 14:08:50 -0500

    mygamecommt2

--- games/mygame.py
+++ games/mygame.py
@@ -3,12 +3,16 @@ import sys
 import random
 import time
 
-
-class Hero:
+class Character:
     def __init__(self, name, x, y):
         self.name = name
-        self.pos = [x, y]
+        self.pos = [x,y]
+        self.speed_x = 0
+        self.speed_y = 0
+        self.pos_ant = None
 
+
+class Hero(Character):
     def mov(self, x, y):
         new_pos = [0,1]
         new_pos[0] = self.pos[0] + x
@@ -28,12 +32,7 @@ class Hero:
             self.pos[1] += y
 
 
-class Monster:
-    def __init__(self, name, x, y):
-        self.name = name
-        self.pos = [x, y]
-        self.pos_ant = None
-
+class Monster(Character):
     def mov(self):
         #Setting random position
         x = random.randint(0, 2)
@@ -41,16 +40,16 @@ class Monster:
         #Changing the direction, and setting the speed
         #for x direction
         if x == 0:
-            x = 30
+            x += 40
         elif x == 1:
-            x = -30
+            x -= -40
         else:
             x = 0
         #for y direction
         if y == 0:
-            y = 30
+            y += 40
         elif y == 1:
-            y = -30
+            y -= -40
         else:
             y = 0
 
@@ -74,7 +73,7 @@ class Monster:
             self.pos[1] = 460
 
         #Just to show the new position
-        print(self.pos)
+        # print(self.pos)
 
 
 def main():
@@ -90,19 +89,17 @@ def main():
     hero_img = pygame.image.load("../images/hero.png")
     monster_img = pygame.image.load("../images/monster.png").convert_alpha()
 
-
     # Game initialization
     hero = Hero('Hero', 236, 220)
     monster = Monster('Monster', 150, 300 )
+
 
     #Using time.time() to wait the next moviment
     time_started = time.time()
     next_action_time = time_started + 2
 
-    #If you hold on the key, you gonna make moviment continuous
-    pygame.key.set_repeat(10)
-
     stop_game = False
+    moving = False
     while not stop_game:
         # look through user events fired
         for event in pygame.event.get():
@@ -112,24 +109,45 @@ def main():
             # to exit the main loop
                 stop_game = True
 
+            if event.type == pygame.KEYDOWN:
+                moving = True
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_UP:
-                    hero.mov(0, -20)
-                elif event.key == pygame.K_DOWN:
-                    hero.mov(0, 20)
-                elif event.key == pygame.K_RIGHT:
-                    hero.mov(20, 0)
-                elif event.key == pygame.K_LEFT:
-                    hero.mov(-20, 0)
+                moving = False
 
+        # print(moving)
+        if moving:
+            if event.key == pygame.K_UP:
+                hero.mov(0, -3)
+            elif event.key == pygame.K_DOWN:
+                hero.mov(0, 3)
+            elif event.key == pygame.K_RIGHT:
+                hero.mov(3, 0)
+            elif event.key == pygame.K_LEFT:
+                hero.mov(-3, 0)
 
         # Game logic
-        if next_action_time <= int(time.time()):
+        if next_action_time <= (time.time()):
             monster.mov()
             time_started = time.time()
-            next_action_time = int(time_started) + 2
+            next_action_time = time_started + 0.4
 
-        pygame.display.update()
+
+        #Decteting the collision
+        #hero_rect position = hero_x, hero_y, hero_width, hero_hight
+        hero_rect = pygame.Rect((hero.pos[0], hero.pos[1], 32, 32))
+        monster_rect = pygame.Rect((monster.pos[0], monster.pos[1], 32, 32))
+        # print(hero_rect)
+        # print(monster_rect)
+        if hero_rect.colliderect(monster_rect):
+
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    pass
+                else:
+                    stop_game = True
+
+
 
         # Draw background
         screen.fill(blue_color)
@@ -138,7 +156,6 @@ def main():
         screen.blit(monster_img, [monster.pos[0], monster.pos[1]])
 
         # Game display
-
         pygame.display.update()
         clock.tick(60)
 
